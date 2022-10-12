8e47528b7bd275e0c9c3464fa8083c74ae153ff5
JeffMacNab24 <jeffreymacnab@gmail.com>
2022-10-11 22:39:09 -0400

    Added interactable 3D camera system

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -36,15 +36,18 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 	m_ImageData = new uint32_t[width * height];
 }
 
-void Renderer::Render()
+void Renderer::Render(const Camera& camera)
 {
+	Ray ray;
+	ray.Origin = camera.GetPosition();
+
 	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
 	{
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
 		{
-			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
-			coord = coord * 2.0f - 1.0f; // -1 -> 1
-			glm::vec4 color = PerPixel(coord);
+			ray.Direction = camera.GetRayDirections()[x + y * m_FinalImage->GetWidth()];
+
+			glm::vec4 color = TraceRay(ray);
 			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
 			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 		}
@@ -53,10 +56,8 @@ void Renderer::Render()
 	m_FinalImage->SetData(m_ImageData);
 }
 
-glm::vec4 Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::TraceRay(const Ray& ray)
 {
-	glm::vec3 rayOrigin(0.0f, 0.0f, 1.0f);
-	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
 	float radius = 0.5f;
 	// rayDirection = glm::normalize(rayDirection);
 
@@ -67,9 +68,9 @@ glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 	// r = radius
 	// t = hit distance
 
-	float a = glm::dot(rayDirection, rayDirection);
-	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
-	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+	float a = glm::dot(ray.Direction, ray.Direction);
+	float b = 2.0f * glm::dot(ray.Origin, ray.Direction);
+	float c = glm::dot(ray.Origin, ray.Origin) - radius * radius;
 
 	// Quadratic forumula discriminant:
 	// b^2 - 4ac
@@ -82,7 +83,7 @@ glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 	float t0 = (-b + glm::sqrt(discriminant)) / (2.0f * a);
 	float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a);
 
-	glm::vec3 hitPoint = rayOrigin + rayDirection * closestT;
+	glm::vec3 hitPoint = ray.Origin + ray.Direction * closestT;
 	glm::vec3 normal = glm::normalize(hitPoint);
 
 	glm::vec3 lightDir = glm::normalize(m_LightDirection);
--- RayTracing/src/Renderer.h
+++ RayTracing/src/Renderer.h
@@ -2,6 +2,9 @@
 
 #include "Walnut/Image.h"
 
+#include "Camera.h"
+#include "Ray.h"
+
 #include <memory>
 #include <glm/glm.hpp>
 
@@ -11,13 +14,13 @@ public:
 	Renderer() = default;
 
 	void OnResize(uint32_t width, uint32_t height);
-	void Render();
+	void Render(const Camera& camera);
 	void SetLightDirection(glm::vec3 lightDir) { m_LightDirection = lightDir; }
 	glm::vec3 GetLightDirection() { return m_LightDirection; }
 
 	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
 private:
-	glm::vec4 PerPixel(glm::vec2 coord);
+	glm::vec4 TraceRay(const Ray& ray);
 private:
 	std::shared_ptr<Walnut::Image> m_FinalImage;
 	uint32_t* m_ImageData = nullptr;
